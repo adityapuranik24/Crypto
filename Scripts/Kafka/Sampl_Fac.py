90b0090bced13c001800b0903be1110907992d49
Aditya Puranik <puranik.aditya.ap@gmail.com>
2024-03-05 19:20:33 -0500

    Transformed live data

--- Scripts/Kafka/Sampl_Fac.py
+++ Scripts/Kafka/Sampl_Fac.py
@@ -1,20 +1,20 @@
 import faust
-
+import pandas as pd
 app=faust.App('demo-streaming',broker='kafka://3.17.23.28:9092')
 
-class Greeting(faust.Record,serializer='json'):
-    name:str
-    age:int
 
-input_topic = app.topic('crypto', value_type=Greeting)
-aged_table = app.Table("major-count",key_type=str,value_type=str,partitions=1,default=int)
+input_topic = app.topic('crypto', value_serializer='json')
+# aged_table = app.Table("major-count",key_type=str, value_type=str, partitions=1, default=int)
 
 
 @app.agent(input_topic)
 async def processor(stream):
     async for message in stream:
-        # if(message.age>30):
-        aged_table[str(message.name)] = message.age
-        print(aged_table.as_ansitable(title='Aged Tabled'))
-        # print(aged_table)
-        
+        rem_list = ['symbol', 'rank', 'color', 'png32', 'png64','webp32', 'webp64', 'pairs', 'categories', 'links', 'delta']
+        for key in rem_list:
+            del message[key]
+        values =pd.DataFrame.from_dict(message, orient='index')
+        print(values)
+
+
+
